f458c81a7e0d110a4edf3abd0fcf2a366ae6d540
vincentkyalomusembi <vincentkyalomusembi@users.noreply.github.com>
2026-07-05 00:12:15 +0300

    finished the farmers module and added the routes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 from app.auth.router import router as auth_router
+from app.farmers.routes import router as farmers_router
 
-app = FastAPI()
+app = FastAPI(
+     title="Agritech AI API",
+    version="2.0.0",
+)
 
+# Register authentication routers
 app.include_router(
     auth_router,
     prefix="/auth",
     tags=["Authentication"],
 )
+
+# Register farmers routers
+app.include_router(farmers_router)
+
+@app.get("/")
+def root():
+    """
+    Health check endpoint.
+    """
+    return {
+        "message": "Agritech AI API is running."
+    }
